5c64b1e645c1396153d3d524ef56947edab8eca4
Nipuna Madula <nipunamadula2001@gmail.com>
2024-09-07 02:08:22 +0530

    Added 'C' button to clear the entry field

--- main.py
+++ main.py
@@ -9,4 +9,8 @@ entry.grid(row=0, column=0, columnspan=4)
 def clear_entry():
     entry.delete(0, tk.END)  # Clear the entry
 
+ # Add 'C' button
+clear_button = tk.Button(root, text="C", padx=20, pady=20, command=clear_entry)
+clear_button.grid(row=4, column=1)
+
 root.mainloop()
